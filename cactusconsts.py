c7de10ed631c63cc3aa657071d1999c7f2dd3b68
636F57 <636f57@outlook.com>
2016-11-22 12:35:52 +0900

    changed to load constants from file
    added cactusconsts.py

---
+++ cactusconsts.py
@@ -0,0 +1,9 @@
+# add whatever constants you want to use in cactusbot.py or cacti.py
+class CactusConsts:
+	Mee6_ID = "xxxxxxxxxxxxxxxxxxxxx"
+	CactusBot_ID = "xxxxxxxxxxxxxxxxxxxx"
+	Slack_Token = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+	CactusBot_Token = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+	cacti_username = r'xxxxxxxxx@xxxx.xx'  #email
+	cacti_password = 'xxxxxxxxxx'          #password
+	
